e8910bd9ab50b7918d54fdfc6b0513472ddbe91d
Ikliltariza <39639994+Ikliltariza@users.noreply.github.com>
2018-05-26 11:58:51 +0800

    Add files via upload

---
+++ Perkalian8.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author H P PAVILION
+ */
+public class Perkalian8 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            int a=8;
+            System.out.println(i + " x " + a + " = " + i*a );
+            i++;
+            
+        }
+    }
+            
+}
